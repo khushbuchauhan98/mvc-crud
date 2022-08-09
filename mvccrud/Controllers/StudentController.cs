eb59e996af994a3bf16eaa528c098de48d9c4831
khushbu19998@outlook.com <khushbu19998@outlook.com>
2022-08-09 14:37:57 +0530

    importing project

---
+++ mvccrud/Controllers/StudentController.cs
@@ -0,0 +1,111 @@
+﻿using mvccrud.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace mvccrud.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        entityEntities3 dbobj = new entityEntities3();
+        
+        public ActionResult Student(tbl_Student stobj)
+        {
+                return View(stobj);
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(tbl_Student model)
+        {
+            tbl_Student stobj = new tbl_Student();
+            if (ModelState.IsValid)
+            {
+
+                stobj.id = model.id;
+                stobj.name = model.name;
+                stobj.fname = model.fname;
+                stobj.email = model.email;
+                stobj.mobile = model.mobile;
+                stobj.description = model.description;
+                if (model.id == 0)
+                {
+                    dbobj.tbl_Student.Add(stobj);
+                    dbobj.SaveChanges();
+                }
+                else
+                {
+                    dbobj.Entry(stobj).State = EntityState.Modified;
+                    dbobj.SaveChanges();
+                }
+
+            }
+            ModelState.Clear();
+            return View("Student");
+        }
+
+        public ActionResult EditStudent(tbl_Student stobj)
+        {
+            //    dbobj.Entry(stobj).State = EntityState.Modified;
+            // dbobj.SaveChanges();
+
+            return View(stobj);
+
+        }
+
+        public ActionResult StudentList()
+        {
+            var res = dbobj.tbl_Student.ToList();
+            return View(res);
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            var res = dbobj.tbl_Student.Where(x => x.id == Id).First();
+            dbobj.tbl_Student.Remove(res);
+            dbobj.SaveChanges();
+            var list = dbobj.tbl_Student.ToList();
+            return View("StudentList",list);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tbl_Student stobj)
+        {
+
+           var user = dbobj.tbl_Student.Where(x => x.name == stobj.name && x.email == stobj.email).Count();
+            if (user > 0)
+            {
+                ModelState.Clear();
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                Response.Write("Somthing Wrong!!");
+                ModelState.Clear();
+                return View();
+            }
+           
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.key = "successfully Login";
+            return View();
+        }
+       
+      
+
+       
+
+
+    }
+
+}
---
+++ mvccrud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvccrud.MvcApplication" Language="C#" %>
